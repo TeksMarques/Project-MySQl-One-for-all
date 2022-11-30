7a3a93d01c005d4201ffe49bdb4b5982236c3199
TeksMarques <taotaodoce@gmail.com>
2022-11-30 17:46:41 -0300

    requisito 4 concluido

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT 
+    u.nome_usuario AS usuario,
+    CASE
+        WHEN YEAR(MAX(h.data_reproducao)) >= 2021 THEN 'Usuário ativo'
+        ELSE 'Usuário inativo'
+    END AS status_usuario
+FROM
+    SpotifyClone.usuarios AS u
+        INNER JOIN
+    SpotifyClone.historico_de_reproducoes AS h ON h.usuario_id = u.usuario_id
+GROUP BY usuario
+ORDER BY usuario;
